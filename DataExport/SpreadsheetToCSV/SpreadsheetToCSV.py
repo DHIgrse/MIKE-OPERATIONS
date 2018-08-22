5551566277bdc76f784171fca88b35132cca3646
DHIgrse <36857267+DHIgrse@users.noreply.github.com>
2018-08-22 15:20:49 +0200

    Create SpreadsheetToCSV.py

---
+++ DataExport/SpreadsheetToCSV/SpreadsheetToCSV.py
@@ -0,0 +1,29 @@
+import clr
+
+clr.AddReference('SpreadsheetGear2017.Core')
+
+from SpreadsheetGear import *
+
+
+
+def SpreadsheetToCSV():
+
+    """
+
+    <Script>
+
+    <Author>AUG</Author>
+
+    <Description>This script exports the active worksheet of a spreadsheet to csv</Description>
+
+    </Script>
+
+    """
+
+    ssMgr = app.Modules.Get('Spreadsheet Manager')
+
+    ss = ssMgr.OpenSpreadsheet('/MySpreadsheet')
+
+    workbook = ss.Workbook
+
+    workbook.SaveAs('c:\\Output\\MySpreadsheet.csv', FileFormat.CSV)
